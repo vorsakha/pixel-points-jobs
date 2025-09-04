b0bd62d1a049441761514eb2aaf03ce764c938fe
Andrei Ferreira <andreitf.dev@gmail.com>
2025-09-04 12:07:37 -0300

    feat: clear errors while connecting | make dev mode on localhost only

--- src/lib/web3/wagmi-config.ts
+++ src/lib/web3/wagmi-config.ts
@@ -2,6 +2,8 @@ import { createConfig, http } from "wagmi";
 import { Chain, mainnet, polygon, polygonMumbai } from "wagmi/chains";
 import { injected, metaMask } from "wagmi/connectors";
 
+const isClient = typeof window !== "undefined";
+
 const localhostChain = {
   id: 31337,
   name: "Localhost",
@@ -17,27 +19,36 @@ const localhostChain = {
   },
 };
 
-const INFURA_API_KEY = process.env.NEXT_PUBLIC_INFURA_API_KEY;
 const PRODUCTION_CHAINS = [mainnet, polygon, polygonMumbai];
-const DEV_CHAINS = [localhostChain, ...PRODUCTION_CHAINS];
+const DEV_CHAINS = [localhostChain];
 
-const chains = (process.env.NODE_ENV === "development"
-  ? DEV_CHAINS
-  : PRODUCTION_CHAINS) as unknown as [Chain, ...Chain[]];
+const chains = (process.env.NODE_ENV === "production"
+  ? PRODUCTION_CHAINS
+  : DEV_CHAINS) as unknown as [Chain, ...Chain[]];
+
+const createTransports = () => {
+  if (process.env.NODE_ENV === "production") {
+    const INFURA_API_KEY = process.env.NEXT_PUBLIC_INFURA_API_KEY;
+    if (!INFURA_API_KEY) {
+      throw new Error("NEXT_PUBLIC_INFURA_API_KEY is required for production");
+    }
+    return {
+      [mainnet.id]: http(`https://mainnet.infura.io/v3/${INFURA_API_KEY}`),
+      [polygon.id]: http(
+        `https://polygon-mainnet.infura.io/v3/${INFURA_API_KEY}`,
+      ),
+      [polygonMumbai.id]: http(
+        `https://polygon-mumbai.infura.io/v3/${INFURA_API_KEY}`,
+      ),
+    };
+  }
+  return {
+    [localhostChain.id]: http("http://127.0.0.1:8545"),
+  };
+};
 
 export const config = createConfig({
   chains,
-  connectors: [injected(), metaMask()],
-  transports: {
-    ...(process.env.NODE_ENV === "development" && {
-      [localhostChain.id]: http("http://127.0.0.1:8545"),
-    }),
-    [mainnet.id]: http(`https://mainnet.infura.io/v3/${INFURA_API_KEY}`),
-    [polygon.id]: http(
-      `https://polygon-mainnet.infura.io/v3/${INFURA_API_KEY}`,
-    ),
-    [polygonMumbai.id]: http(
-      `https://polygon-mumbai.infura.io/v3/${INFURA_API_KEY}`,
-    ),
-  },
+  connectors: [injected(), ...(isClient ? [metaMask()] : [])],
+  transports: createTransports(),
 });
